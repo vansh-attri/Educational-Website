05c022998e5955abbfb67dc34e2cad4d91bfd986
Vansh Attri <vanshattri123@gmail.com>
2023-12-06 12:44:05 +0530

    Made form working

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ navOpenBtn.addEventListener("click", () => {
 navCloseBtn.addEventListener("click", () => {
   nav.classList.remove("openNav");
 }); 
+
+function sendemail() {
+  Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "bdcommerce09@gmail.com",
+      Password: "7247E6A8582F935491EC67310B93F9CB4EA8",
+      To: 'bdcommerce09@gmail.com',
+      From: 'bdcommerce09@gmail.com',
+      Subject: "Message from Website",
+      Body: "Name: " + document.getElementById("name").value
+      + " <br> Email: " + document.getElementById('email').value
+      + " <br> Phone No: " + document.getElementById('phone').value
+      + " <br> Message: " + document.getElementById('message').value
+  }).then(
+      message => alert(message)
+  );
+}
